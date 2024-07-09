7dfa5684aa934691e6c1288ec5a72f5b7b1cdc1a
bonxbons <144151209+bonxbons@users.noreply.github.com>
2024-07-09 02:23:46 -0400

    finished starter script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,3 +4,11 @@ script that is starting a Flask web application
 """
 
 from web_flask import app
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
