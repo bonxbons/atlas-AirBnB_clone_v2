06fa73c0e43d6259a774b8d82ec9c0872b597646
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-07-10 04:49:51 -0500

    rewriting 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,24 +5,21 @@ A script that starts a Flask web application
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage session"""
+def close_storage(exception):
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State).values()  # Fetch all State objects
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = storage.all('State').values()
+    states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
