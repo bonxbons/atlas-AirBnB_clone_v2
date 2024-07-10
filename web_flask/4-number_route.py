721cdda4f5fd8b4b1ffa22be794a389f83b2dd00
bonxbons <144151209+bonxbons@users.noreply.github.com>
2024-07-10 00:10:45 -0400

    couldnt import flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -3,9 +3,8 @@
 A script that starts a Flask web application
 """
 
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
